4336cf7d4833e6b54909e4586d3ee6d98c64d3ea
John Sullivan <jsull003@ucr.edu>
2013-02-04 18:13:30 -0800

    Initial commit.
    * I don't like the interface or look of this code very much. Expect
      changes in both.

---
+++ galah_interact/__init__.py
@@ -0,0 +1,3 @@
+from models import GalahTestPart, GalahTest, GalahResult, GalahConfig
+from testrunner import TestRunner, TestDescription
+import common
---
+++ galah_interact/mirroredmodels.py
@@ -0,0 +1,3 @@
+# TODO: Create mirrors of the models inside of galah.db.models that don't depend
+#       on mongoengine being available. Add support for deserialization to each
+#       of these. Serialization is unnecessary.
