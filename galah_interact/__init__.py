00b73053a777903725c4466d5fdbba27fe9673a3
brownhead <jsull003@ucr.edu>
2013-05-01 16:09:57 -0700

    Removed old interact code.

--- galah_interact/__init__.py
+++
@@ -1,3 +0,0 @@
-from models import GalahTestPart, GalahTest, GalahResult, GalahConfig
-from testrunner import TestRunner, TestDescription
-import common
--- galah_interact/mirroredmodels.py
+++
@@ -1,3 +0,0 @@
-# TODO: Create mirrors of the models inside of galah.db.models that don't depend
-#       on mongoengine being available. Add support for deserialization to each
-#       of these. Serialization is unnecessary.
