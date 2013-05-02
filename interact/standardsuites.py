d62b9e29e619fab741f59bcb243ee01d12673482
brownhead <jsull003@ucr.edu>
2013-05-01 22:40:34 -0700

    Moved check_compiles code into standardsuites.

--- interact/standardsuites.py
+++ interact/standardsuites.py
@@ -29,6 +29,13 @@ import interact
 import interact._utils as _utils
 import re
 from parsing import Block, Line, grab_blocks
+import interact.core
+import interact._utils as _utils
+import tempfile
+import subprocess
+import os.path
+import atexit
+import shutil
 
 def check_files_exist(*files):
     """
@@ -143,3 +150,42 @@ def indentation(code, file_name, max_score = 10, allow_negative = False):
     result.score = score
 
     return result
+
+def check_compiles(files, flags = [], ignore_cache = False):
+    # Try to compile the program
+    compiler_output, executable_path = compile_program(
+        files, flags = flags, ignore_cache = ignore_cache
+    )
+
+    command = _utils.craft_shell_command(create_compile_command(files, flags))
+    result = interact.core.TestResult(
+        brief = "This test ensures that your code compiles without errors. "
+                "Your program was compiled with %s." % (command, ),
+        default_message = "**Great job!** Your code compiled cleanly without "
+                          "any problems.",
+        max_score = 10,
+        bulleted_messages = False
+    )
+
+    # If the compilation failed
+    if executable_path is None:
+        if compiler_output:
+            compiler_output = _utils.truncate_string(compiler_output)
+
+            result.add_message(
+                "Your code did not compile. The compiler output the following "
+                "errors:\n\n```\n{compiler_output}\n```\n",
+                compiler_output = compiler_output,
+                dscore = -10
+            )
+        else:
+            result.add_message(
+                "Your code did not compile but the compiler did not output "
+                "any errors or warnings."
+            )
+
+        result.score = 0
+    else:
+        result.score = 10
+
+    return result;
