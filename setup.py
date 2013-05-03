26d095a39563cad1e49780c622091158d5b84711
brownhead <jsull003@ucr.edu>
2013-05-03 01:53:32 -0700

    Added manifest file. Setup now uses find_packages

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import os
-from setuptools import setup
+from setuptools import setup, find_packages
 
 # Utility function to read the README file.
 # Used for the long_description.  It's nice, because now 1) we have a top level
@@ -22,7 +22,7 @@ setup(
     license = "Apache v2.0",
     keywords = "education framework",
     url = "https://www.github.com/galah-group/galah-interact-python",
-    packages = ["interact", "interact_test"],
+    packages = find_packages(),
     long_description = read("README.md"),
     classifiers = [
         "Development Status :: 3 - Alpha",
